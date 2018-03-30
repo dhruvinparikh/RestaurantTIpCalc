10b0c71c9e0f3378be47fb65b895dedc2a9381cb
Dhruvin <n01150847@humbermail.ca>
2018-03-29 22:19:33 -0400

    Tip slider default value=18% and minor fixes

--- RestaurantTIpCalc/ViewController.swift
+++ RestaurantTIpCalc/ViewController.swift
@@ -10,27 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    //Bill Amount field
     @IBOutlet weak var billTF: UITextField!
-    
     @IBOutlet weak var slidetipvallbl: UILabel!
     @IBOutlet weak var tipVallbl: UILabel!
     @IBOutlet weak var tipPerlbl: UILabel!
     @IBOutlet weak var tipLbl: UILabel!
     @IBOutlet weak var totalvallbl: UILabel!
     @IBOutlet weak var totalslidevallbl: UILabel!
+    @IBOutlet weak var lblTipSlider: UISlider!
+    
+    //Called when billa amount is edited
     @IBAction func editingchangeBill(_ sender: UITextField) {
         tipVallbl.text = String(0.15 * Float(billTF.text!)!)
         totalvallbl.text = String(0.15 * Float(billTF.text!)! + Float(billTF.text!)!)
+        slidetipvallbl.text = String((lblTipSlider.value/100.0) * Float(billTF.text!)!)
+        totalslidevallbl.text = String((lblTipSlider.value/100.0) * Float(billTF.text!)! + Float(billTF.text!)!)
+        
     }
     @IBAction func billValueChnage(_ sender: Any) {
-        tipVallbl.text = String(0.15 * Float(billTF.text!)!)
-        totalvallbl.text = String(0.15 * Float(billTF.text!)! + Float(billTF.text!)!)
     }
     @IBAction func tipSlider(_ sender: Any) {
         
     }
-    
+    //called when horizontal slider is changed
     @IBAction func tipSlide(_ sender: UISlider) {
         tipLbl.text = String(sender.value)+"%"
         tipPerlbl.text = String(sender.value)+"%"
@@ -44,13 +47,15 @@ class ViewController: UIViewController {
         }
         
     }
-    
+    //called during init of app
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
             tipVallbl.text = String(0.00)
             totalvallbl.text = String(0.00)
+        tipPerlbl.text = String(lblTipSlider.value)+"%"
+        tipLbl.text = String(lblTipSlider.value)+"%"
     }
 
     override func didReceiveMemoryWarning() {
